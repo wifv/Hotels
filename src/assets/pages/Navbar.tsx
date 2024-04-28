1c4104aa6717a0dd11c9c7cb8b9b8de488d9b611
wifv <abcogenshin@gmail.com>
2024-04-28 18:20:23 +0500

    l

---
+++ src/assets/pages/Navbar.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import '../css/navbar.css'
+import menu from '../images/menu.svg'
+
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false)
+  
+  function show () {
+    setShowMenu(!showMenu)
+  }
+
+  return (
+    <div className="container">
+      <nav className="nav">
+          <a className="logo" href='/'>My Portfolio</a>
+          <ul className={showMenu ? "active-nav-right" : "nav-right"} id='nav-right'>
+              <li className="nav-item"><a href='/about'>About</a></li>
+              <li className="nav-item"><a href='/contact'>Contact</a></li>
+          </ul>
+          <img src={menu} alt="navigation menu" className='menu' onClick={show}/>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
