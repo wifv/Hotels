8e877a20a22b703bde48b6a5cd360ae79d82af23
wifv <abcogenshin@gmail.com>
2024-05-11 20:10:22 +0500

    lol

--- src/assets/pages/Click.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var clickHandler = function clickHandler(setTheme, theme) {
-  return function (event) {
-    setTheme(!theme);
-    event.preventDefault();
-  };
-};
--- src/assets/pages/Navbar.tsx
+++ src/assets/pages/Navbar.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import '../css/navbar.css'
-import theme from '../theme.svg'
+// import theme from '../theme.svg'
 
 const Navbar = () => {
-  const [dark, setDark] = useState(false)
+  // const [dark, setDark] = useState(false)
 
   return (
-    <div className={dark ? "dark container-nav" : "light container-nav"}>
+    // <div className={dark ? "dark container-nav" : "light container-nav"}>
+    <div className="container-nav">
       <nav className="nav">
           <a className="logo" href='/'>Davlet kak dela</a>
           <ul className="nav-right" id='nav-right'>
