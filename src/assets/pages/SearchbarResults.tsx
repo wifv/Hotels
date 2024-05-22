7c38e65dc762e6934d5d186eb032f3319480d7f0
wifv <abcogenshin@gmail.com>
2024-05-22 19:56:46 +0500

    done

--- src/assets/pages/SearchbarResults.tsx
+++ src/assets/pages/SearchbarResults.tsx
@@ -4,14 +4,14 @@ const SearchbarResults = ({results}: any) => {
   return (
     <div className='results'>
       {
-        results.length >= 15
-        ?
-        results.slice(0, 15).map((item: any, id: any) => {
-          return <div key={id} className='results-item'>{item.title}</div>
-        })
-        :
+        // results.length >= 15
+        // ?
+        // results.slice(0, 15).map((item: any, id: any) => {
+        //   return <div key={id} className='results-item'>{item.title}</div>
+        // })
+        // :
         results.map((item: any, id: any) => {
-          return <div key={id}>hey {item.title}</div>
+          return <div key={id} className='results-item'>{item.title}</div>
         })
       }
     </div>
